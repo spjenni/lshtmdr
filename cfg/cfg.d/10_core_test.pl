9edbd31c39e854c52e86c8369cc6a210dba44f8c
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2015-02-05 16:32:30 -0800

    local only for this file

--- cfg/cfg.d/10_core_test.pl
+++
@@ -1,9 +0,0 @@
-# This file was created by bin/epadmin
-# You can regenerate this file by doing ./bin/epadmin config_core template
-
-$c->{host} = 'eprints.example.my';
-$c->{port} = 8081;
-$c->{aliases} = [];
-$c->{securehost} = '';
-$c->{secureport} = 443;
-$c->{http_root} = undef;
