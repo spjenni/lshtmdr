9d13ca682c4b648d18faa8a0fa0cbf0e345feb4e
spjenni <simon.jennings@lshtm.ac.uk>
2015-02-25 15:18:05 +0000

    git ignore and eprints fields

--- cfg/cfg.d/10_core_test.pl
+++ cfg/cfg.d/10_core_test.pl
@@ -1,6 +1,7 @@
 # This file was created by bin/epadmin
 # You can regenerate this file by doing ./bin/epadmin config_core template
-$c->{host} = 'plug.ep.devorkin';
+
+$c->{host} = 'eprints.example.my';
 $c->{port} = 8081;
 $c->{aliases} = [];
 $c->{securehost} = '';
