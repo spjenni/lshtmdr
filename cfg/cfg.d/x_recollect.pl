ad8bc05e17b00417840c0df1dc3312360c52fab2
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2015-03-18 01:15:53 -0700

    name_and_lshtmid cgi and jquerymd5

--- cfg/cfg.d/x_recollect.pl
+++ cfg/cfg.d/x_recollect.pl
@@ -14,11 +14,39 @@ $c->{recollect_set_eprint_defaults} = $c->{set_eprint_defaults};
 $c->{set_eprint_defaults} = sub
 {
 	my( $data, $repository ) = @_;
+	print STDERR "#########################".$data."#############################\n";
 	$repository->call("recollect_set_eprint_defaults");
 	if(!EPrints::Utils::is_set( $data->{type} ))
 	{
 		$data->{type} = "data_collection";	
 	}
+	
+	my $user = $repository->current_user;
+	if( defined $user )
+	{
+    		#display initials, but don't store initials....
+    		#my $initials = $user->get_value( 'name' )->{given};
+     		#   $initials =~ s/^(\w)[^\s]*(|\s+(\w)[^\s]*(|\s+(\w)[^\s]*))$/$1$3$5/; #no more than 3 initials...
+   		 
+		$data->{creators} = [ {
+			name => {given => $user->get_value( 'name' )->{given}, family => $user->get_value( 'name' )->{family}, honourific => $user->get_value( 'name' )->{honourific},
+        		lineage => $user->get_value( 'name')->{lineage}},
+ 			id => $user->get_value( 'email' ),
+            		lshtmid => $user->get_value( 'lshtmid' ),
+        		orcid => $user->get_value( 'orcid' )
+        	} ];
+        
+		$data->{creators}->[0]->{lshtm_flag} = "TRUE" if(defined $data->{creators}->[0]->{lshtmid});
+		
+		$data->{contact_email}  = $user->get_value( 'email' );
+	}
+		
+    	$data->{corp_creators} = [$repository->phrase("default_corp_creators_value")];
+   	$data->{publisher} = $repository->phrase("default_publisher_value");
+    	$data->{place_of_pub} = $repository->phrase("default_place_of_pub_value");
+    	$data->{copyright_holders} = [$repository->phrase("default_copyright_holders_value")];
+    	$data->{language} = [{ l => $repository->phrase("default_language_value")}];
+
 };
 
 #### add automatic values for publisher and date
