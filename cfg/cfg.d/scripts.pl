900412ae4fc82915efda004b5a9119711bcf1858
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2015-03-31 04:55:45 -0700

    run_export_bar and run_coduments_recollect

--- cfg/cfg.d/scripts.pl
+++ cfg/cfg.d/scripts.pl
@@ -14,3 +14,64 @@ sub run_current_user
 
 	return [ $state->{session}->current_user(),  "STRING" ];
 }
+
+sub run_export_bar
+{
+	my( $self, $state, $eprint ) = @_;
+
+	if( ! $eprint->[0]->isa( "EPrints::DataObj::EPrint") )
+	{
+		$self->runtime_error( "documents() must be called on an eprint object." );
+	}
+	$eprint = $eprint->[0];
+	my $repository = $state->{session}->get_repository();
+	my $export_bar = $repository->make_element( "div", style => "ep_block" );
+	
+	my @plugins = $repository->get_plugins(
+	    type => "Export",
+	    can_accept => "dataobj/eprint",
+	    is_advertised => 1,
+	    is_visible => "all" );
+	
+	my $uri = $repository->get_url( path => "cgi" ) . "/export_redirect";
+	my $form = $repository->render_form( "GET", $uri );
+	$export_bar->appendChild( $form );
+	$form->appendChild( $repository->render_hidden_field( dataobj => $eprint->id ) );
+	
+	my $select = $repository->make_element( "select", name => "format" );
+	$form->appendChild( $select );
+	
+	foreach my $plugin (@plugins)
+	{
+	    my $plugin_id = $plugin->get_id;
+	    $plugin_id =~ s/^Export:://;
+	    my $option = $repository->make_element( "option", value => $plugin_id );
+	    $select->appendChild( $option );
+	    $option->appendChild( $plugin->render_name );
+	}
+	my $button = $repository->make_element( "input", type => "submit", value => $repository->phrase( "lib/searchexpression:export_button" ), class => "ep_form_action_button" );
+	$form->appendChild( $button );
+	
+	return [ $export_bar, "XHTML" ];
+}
+
+sub run_documents_recollect
+{
+	my( $self, $state, $eprint, $format ) = @_;
+
+	if( ! $eprint->[0]->isa( "EPrints::DataObj::EPrint") )
+	{
+		$self->runtime_error( "documents() must be called on an eprint object." );
+	}
+	$format = $format->[0];
+	$eprint = $eprint->[0];
+
+	my $sorteddocs = [];
+	for my $doc($eprint->get_all_documents()){
+		next if($format ne $doc->value("format"));
+		push @$sorteddocs, $doc;
+	}
+	return [ [@$sorteddocs],  "ARRAY" ];
+
+}
+
