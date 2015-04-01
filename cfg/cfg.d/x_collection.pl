71f25bc75190def2c134c58ba333a79579ad5fb1
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2015-03-31 20:39:31 -0700

    added collection_render sub

--- cfg/cfg.d/x_collection.pl
+++ cfg/cfg.d/x_collection.pl
@@ -61,17 +61,98 @@ $c->{session_init} = sub {
 $c->{collection_eprint_render} = $c->{eprint_render};
 
 #overwrite collection_render in order to make a custom render method for collections
-$c->{collection_render} = $c->{eprint_render};
+$c->{collection_render} = sub {
+
+	my ($eprint, $repository, $preview) = @_;
+
+####### All this nonsense ######
+
+	my $succeeds_field = $repository->dataset( "eprint" )->field( "succeeds" );
+	my $commentary_field = $repository->dataset( "eprint" )->field( "commentary" );
+
+	my $flags = { 
+		has_multiple_versions => $eprint->in_thread( $succeeds_field ),
+		in_commentary_thread => $eprint->in_thread( $commentary_field ),
+		preview => $preview,
+	};
+	my %fragments = ();
+
+	# Put in a message describing how this document has other versions
+	# in the repository if appropriate
+	if( $flags->{has_multiple_versions} )
+	{
+		my $latest = $eprint->last_in_thread( $succeeds_field );
+		if( $latest->value( "eprintid" ) == $eprint->value( "eprintid" ) )
+		{
+			$flags->{latest_version} = 1;
+			$fragments{multi_info} = $repository->html_phrase( "page:latest_version" );
+		}
+		else
+		{
+			$fragments{multi_info} = $repository->render_message(
+				"warning",
+				$repository->html_phrase( 
+					"page:not_latest_version",
+					link => $repository->render_link( $latest->get_url() ) ) );
+		}
+	}		
+
+
+	# Now show the version and commentary response threads
+	if( $flags->{has_multiple_versions} )
+	{
+		$fragments{version_tree} = $eprint->render_version_thread( $succeeds_field );
+	}
+	
+	if( $flags->{in_commentary_thread} )
+	{
+		$fragments{commentary_tree} = $eprint->render_version_thread( $commentary_field );
+	}
+
+
+	foreach my $key ( keys %fragments ) { $fragments{$key} = [ $fragments{$key}, "XHTML" ]; }
+	
+
+	my $title = $eprint->render_citation("brief");
+
+	my $links = $repository->xml()->create_document_fragment();
+	if( !$preview )
+	{
+		$links->appendChild( $repository->plugin( "Export::Simple" )->dataobj_to_html_header( $eprint ) );
+		$links->appendChild( $repository->plugin( "Export::DC" )->dataobj_to_html_header( $eprint ) );
+	}
+
+###### Is there because... #####
+	my $page = $eprint->render_citation( "collection_item", %fragments, flags=>$flags );
+
+
+###### This bit doesn't really work : gives a Template not loaded error #######
+	#to define a specific template to render the abstract with, you can do something like:
+	# my $template;
+	# if( $eprint->value( "type" ) eq "article" ){
+	# 	$template = "article_template";
+	# }
+	# return ( $page, $title, $links, $template );
+
+###### In theory we could remove most of the above and just ... #######
+    	# my ($page, $title, $links) = $repository->call("recollect_eprint_render", $eprint, $repository, $preview);
+	#return( $page, $title, $links "recollect_summary_page");
+
+####### But... #######
+	return( $page, $title, $links );
+
+
+};
 
 $c->{eprint_render} = sub
 {
-        my( $eprint, $session, $preview ) = @_;
-	
+	my ($eprint, $repository, $preview) = @_;
+
 	if( $eprint->value("type") ne "collection" )
 	{
-        	return $session->get_repository->call("collection_eprint_render", $eprint, $session, $preview );
+        	return $repository->call("collection_eprint_render", $eprint, $repository, $preview );
 	}
 	
-        return $session->get_repository->call("collection_render", $eprint, $session, $preview );
+	return $repository->call("collection_render", $eprint, $repository, $preview );
 };
 
