09769d250b36139c93f922556b0583e600f48a66
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2015-04-19 23:20:07 -0700

    datacompass domain

--- cfg/cfg.d/10_core.pl
+++ cfg/cfg.d/10_core.pl
@@ -1,6 +1,6 @@
 # This file was created by bin/epadmin
 # You can regenerate this file by doing ./bin/epadmin config_core template
-$c->{host} = 'w01.lshtmdr.da.ulcc.ac.uk';
+$c->{host} = 'datacompass.lshtm.ac.uk';
 $c->{port} = 80;
 $c->{aliases} = [];
 $c->{securehost} = '';
