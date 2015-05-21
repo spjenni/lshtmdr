bea0dc8fbcd8890a5b3d5a75161495652ff083d5
spjenni <simon.jennings@lshtm.ac.uk>
2015-05-21 14:20:12 +0100

    change to auto check lshtmid flag function, now uses family rather then lshtmid to check the flag on change of table row, not sure if this is correct but wasnt functioning before

--- cfg/static/javascript/auto/90_local.js
+++ cfg/static/javascript/auto/90_local.js
@@ -49,16 +49,17 @@ function auto_check_lshtmid_flag(){
 //  jQuery('input:regex(id,c[0-9]+_.+(|_[0-9]+)_lshtmid)').on("change", function(){   
   //  jQuery("table.ep_form_input_grid").on("change", 'input:regex(id,c[0-9]+_.+(|_[0-9]+)_lshtmid)', function() {
   jQuery("table.ep_form_input_grid").on("change", 'input', function() {
-
-    if(jQuery(this).attr("id").match(/c[0-9]+_.+(|_[0-9]+)_lshtmid/)){
-   
+    
+    if(jQuery(this).attr("id").match(/c[0-9]+_.+(|_[0-9]+)_family/)){
+			
             var id = jQuery(this).attr("id");
-            var flag_id = id.replace("lshtmid", "lshtm_flag");
+            var flag_id = id.replace("name_family", "lshtm_flag");
+            
             if(jQuery(this).val().length == 0){
-              jQuery("#"+flag_id).prop('checked', false);
-        }else{
-              jQuery("#"+flag_id).prop('checked', true);
-        }
+				jQuery("#"+flag_id).prop('checked', false);
+			}else{
+				jQuery("#"+flag_id).prop('checked', true);
+      }
     }
   });
 }
